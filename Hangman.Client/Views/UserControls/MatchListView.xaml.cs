5ed41f89cd3b1937f88e9bd1d6ebf4b5ee91e765
MZSM98 <marcoszenon98@gmail.com>
2026-06-05 19:30:23 -0600

    Add: LoginView, Styles

---
+++ Hangman.Client/Views/UserControls/MatchListView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hangman.Client.Views.UserControls
+{
+    /// <summary>
+    /// Lógica de interacción para MatchListView.xaml
+    /// </summary>
+    public partial class MatchListView : Window
+    {
+        public MatchListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
